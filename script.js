179dfd8eaa4a2fbc52fe144c21a479469c3e8d9b
renal24675 <82034892+renal24675@users.noreply.github.com>
2023-02-27 03:24:19 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const navbarNav = document.querySelector('.navbar-nav');
+
+//ketika menu di klick
+
+document.querySelector ('.opsi').
+onclick = ( ) => {
+    navbarNav.classList.toggle ('active');
+};
