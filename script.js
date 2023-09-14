b70ef0ef16ce1d72fdb8acc7d197c447633e2652
renal24675 <82034892+renal24675@users.noreply.github.com>
2023-09-14 07:30:13 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-const navbarNav = document.querySelector('.navbar-nav');
-
-//ketika menu di klick
-
-document.querySelector ('.opsi').
-onclick = ( ) => {
-    navbarNav.classList.toggle ('active');
-};
